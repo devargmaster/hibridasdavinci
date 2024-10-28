e1c9b2ea5e9e090f7a4a66bb4add35831786e9c9
Walter Arce <142426140+devargmaster@users.noreply.github.com>
2024-10-28 11:37:30 -0300

    feat:cambios en controlador

--- desafio5/controllers/libros.controller.js
+++ desafio5/controllers/libros.controller.js
@@ -1,7 +1,10 @@
 class LibrosController {
     async  find(req, res) {
-
         res.send('Lista de libros');
+    }
+    async findbyId(id){
+        const rid = req.params.id;
+        res.send(`Id ${id}`)
     }
 }
 
